c32f35e7bb39c4d0c60893b7615525c3a799a1ab
wallleap <15579576761@163.com>
2019-09-17 23:36:28 +0800

    update

---
+++ js/title.js
@@ -0,0 +1,1 @@
+var OriginTitile=document.title,st;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="看不见我🙈~看不见我🙈~",clearTimeout(st)):(document.title="(๑•̀ㅂ•́) ✧被发现了～",st=setTimeout(function(){document.title=OriginTitile},3e3))})
