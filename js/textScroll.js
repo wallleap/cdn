44e56cbd573e5238029d1779b249164388e57f3c
wallleap <15579576761@163.com>
2020-03-12 16:56:08 +0800

    update

--- js/textScroll.js
+++ js/textScroll.js
@@ -17,7 +17,7 @@
       c.step ? c.step-- : (c.step = g, c.prefixP < l.length ? (c.prefixP >= 0 && (c.text += l[c.prefixP]), c.prefixP++) : "forward" === c.direction ? c.skillP < t.length ? (c.text += t[c.skillP], c.skillP++) : c.delay ? c.delay-- : (c.direction = "backward", c.delay = a) : c.skillP > 0 ? (c.text = c.text.slice(0, -1), c.skillP--) : (c.skillI = (c.skillI + 1) % o.length, c.direction = "forward")), r.textContent = c.text, r.appendChild(n(c.prefixP < l.length ? Math.min(s, s + c.prefixP) : Math.min(s, t.length - c.skillP))), setTimeout(i, d)
     }
     var l = "",
-    o = ["青青陵上柏，磊磊涧中石。", "人生天地间，忽如远行客。","斗酒相娱乐，聊厚不为薄。", "驱车策驽马，游戏宛与洛。","洛中何郁郁，冠带自相索。","长衢罗夹巷，王侯多第宅。","两宫遥相望，双阙百余尺。","极宴娱心意，戚戚何所迫？"].map(function (r) {
+    o = ["懒惰是很奇怪的东西，它使你以为那是安逸，是休息，是福气；", "但实际上它所给你的是无聊，是倦怠，是消沉；","它剥夺你对前途的希望，割断你和别人之间的友情，使你心胸日渐狭窄，对人生也越来越怀疑。", "脚步的坚实，不在于穿多重的鞋，而是看是否经历过陡峭；","内心的强大，不在于心里想的多，而是看是否经历过波折；","梦想的实现，不在于有多少收获，而是看如何击败了重重困难，最终得到好的结果。","不要因为没有阳光，而走不进春天不要因为没有歌声，而放弃自己的追求不要因为没有掌声，而丢掉自己的理想。","其实每一条都通往阳光的大道，都充满坎坷每一条通向理想的途径，都充满了艰辛与汗水！"].map(function (r) {
     return r + ""
     }),
     a = 2,
